4e17febc8c80d42a939f26538e0cb077f019327d
coderchrisdean <coderchrisdean@gmail.com>
2023-04-01 15:36:11 -0700

    added dashboard modal for a cooler look, added dashboard-routes controller, updated server.js by moving handling to home-routes controller

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-  res.send('This is the dashboard route.');
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      }
+    });
+    const posts = postData.map(post => post.get({ plain: true }));
+    res.render('dashboard', { 
+      layout: 'dashboard', 
+      posts, 
+      loggedIn: true 
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
